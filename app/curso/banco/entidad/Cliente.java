39e2623eae7753854b6d996e328ce5094871ebe7
SergioCozarCurso <sergiocozar.r+1@gmail.com>
2022-10-26 18:33:09 +0200

    first changes

---
+++ app/curso/banco/entidad/Cliente.java
@@ -0,0 +1,12 @@
+package app.curso.banco.entidad;
+
+public class Cliente extends Persona{
+
+	public Cliente() {
+		
+	}
+	
+	public Cliente(int i, String nombre, String telefono) {
+		super(i, nombre, telefono);
+	}
+}
---
+++ app/curso/banco/entidad/Persona.java
@@ -0,0 +1,52 @@
+package app.curso.banco.entidad;
+
+public class Persona {
+	
+	protected int id;
+	protected String name;
+	protected String phone;
+	
+	Persona(){
+		
+	}
+	
+	Persona(int id, String nombre, String telefono) {
+		this.id = id;
+		this.name = nombre;
+		this.phone = telefono;
+	}
+	
+	public void mostrarInfo () {
+		System.out.println("Id: " + this.id);
+		System.out.println("Nombre: " + this.name);
+		System.out.println("Número de Teléfono: " + this.phone);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+	
+
+}
