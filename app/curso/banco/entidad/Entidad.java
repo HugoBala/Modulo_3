96da27c243f69cd3eb7d01314c68bdf00279a01d
SergioCozarCurso <sergiocozar.r+1@gmail.com>
2022-10-26 17:34:21 +0200

    packages creados

--- app/curso/banco/entidad/Entidad.java
+++ app/curso/banco/entidad/Entidad.java
@@ -1,11 +1,9 @@
-package prueba;
+package app.curso.banco.entidad;
 
-public class Main {
+public class Entidad {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("Test");
 
 	}
 
